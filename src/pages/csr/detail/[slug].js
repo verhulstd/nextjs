ea0aa6d61bc12a39a2c83a6a6819db56d41691ec
Verhulst David (p130024) <david.verhulst@ap.be>
2023-05-16 13:57:50 +0200

    update

--- src/pages/csr/detail/[slug].js
+++ src/pages/csr/detail/[slug].js
@@ -1,14 +1,37 @@
+/* eslint-disable @next/next/no-img-element */
 import { useRouter } from "next/router";
-
+import { useState, useEffect } from "react";
 const Detail = () => {
-  const {
-    query: { slug },
-  } = useRouter();
-  const id = slug.split("-")[0];
+  const router = useRouter();
+  const [cocktail, setCocktail] = useState({
+    loading: false,
+    data: {},
+  });
+  useEffect(() => {
+    (async () => {
+      if (router.query.slug) {
+        setCocktail({ loading: true, data: {} });
+        const response = await fetch(
+          `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${
+            router.query.slug.split("-")[0]
+          }`
+        );
+        const { drinks } = await response.json();
+        setCocktail({ loading: false, data: drinks[0] });
+      }
+    })();
+  }, [router]);
   return (
     <>
       <h1>Detail of Cocktail using CSR</h1>
-      <h2>id : {id}</h2>
+      {cocktail.loading && <p>LOADING...</p>}
+      {cocktail.data && (
+        <>
+          <h2>{cocktail.data.strDrink}</h2>
+          <img src={cocktail.data.strDrinkThumb} alt={cocktail.data.strDrink} />
+          <p>{cocktail.data.strInstructions}</p>
+        </>
+      )}
     </>
   );
 };
