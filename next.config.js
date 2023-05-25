c7e2d08c39a16951373e5581c4ec336fe3d0679e
David Verhulst <info@inventivemedia.be>
2023-05-25 20:09:11 +0200

    wrong domain

--- next.config.js
+++ next.config.js
@@ -5,9 +5,9 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "s3.syntradeveloper.be",
+        hostname: "images.unsplash.com",
         port: "",
-        pathname: "/images/*",
+        pathname: "/*",
       },
     ],
   },
